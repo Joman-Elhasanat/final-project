f5e4193208d61653c206e7d15661e4be42514e79
Joman-Elhasanat <Joman409259215@.com>
2025-02-21 21:38:11 +0200

    Add tasks view and create tasks migration, update routes

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,46 @@
 <?php
-
-use Carbon\Factory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', action: function(): Factory|View {
+
+Route::get('/about', function(): Factory|View {
     $name = 'joman';
     $departments = [
         '1' => 'Technical',
         '2' => 'Financial',
         '3' => 'Sales',
     ];
-    //return view('about', ['name' => $name]);
-   //return view('about',->with('name' => $name);
-    return view(view: 'about', data: compact('name', 'departments'));
+    return view('about', compact('name', 'departments'));
 });
 
-Route::post(uri: '/about', action: function(): Factory|View {
-    $name = $_POST['name'];
+Route::post('/about', function(): Factory|View {
+    $name = request()->input('name');
     $departments = [
         '1' => 'Technical',
         '2' => 'Financial',
         '3' => 'Sales',
     ];
-    return view(view: 'about', data: compact('name', 'departments'));
+    return view('about', compact('name', 'departments'));
+});
+
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('tasks'));
+});
+
+Route::post('/create', function () {
+    $task_name = request()->input('name');
+    DB::table('tasks')->insert(['name' => $task_name]);
+    return redirect()->back();
+});
+
+Route::delete('/delete/{id}', function ($id): RedirectResponse {
+    DB::table('tasks')->where('id', $id)->delete();
+    return redirect()->back();
 });
